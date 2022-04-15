dab54fb0497ee6082947b5f382e68dea659d9a40
Flandi000 <103685291+Flandi000@users.noreply.github.com>
2022-04-15 13:32:23 +0700

    Update Cubixbot.py

--- Cubixbot.py
+++ Cubixbot.py
@@ -36,14 +36,6 @@ async def Sendhentai(ctx):
             b = await response.json()
             await ctx.send(b.get("url"))
             
-@bot.command(name="Github")
-async def sendgithub(ctx):
-    embed = discord.Embed(
-        title="Ссылка",
-        description="Ссылка для перехода на Githab",
-        url='https://github.com/Flandi000/Cubixbot',
-    )
-    await ctx.send(embed=embed)
 
 @bot.event
 async def on_message(message):
